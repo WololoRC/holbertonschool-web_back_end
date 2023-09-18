f2e530db11f4d203e9394c19c779c499a8105c9f
WololoRC <wololoRC@proton.me>
2023-09-18 12:30:34 -0300

    very pycose style

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""
+A type-annotated function add that
+takes a float a and a float b as arguments
+and returns their sum as a float.
+"""
+
 
 def add(a: float, b: float) -> float:
     """
-    A type-annotated function add that
-    takes a float a and a float b as arguments
-    and returns their sum as a float.
+    add two floats and return a float
     """
     return a + b

f46a210c586714e4f7a3eff4604c41fa7eca0321
WololoRC <wololoRC@proton.me>
2023-09-18 12:42:24 -0300

    tsk2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function floor which takes
+a float n as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns de floor of a float
+    """
+    return int(n)

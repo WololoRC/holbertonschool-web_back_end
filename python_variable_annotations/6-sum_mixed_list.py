f03472972b35bebd9d5e3ec15e0ca43ce0da2505
WololoRC <wololoRC@proton.me>
2023-09-18 13:25:58 -0300

    tsk6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function sum_mixed_list which
+takes a list mxd_lst of integers and floats
+and returns their sum as a float.
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: [Union[int, float]]) -> float:
+    """
+    Return the sum of mxd_list as a float
+    """
+    result: float = 0
+
+    for item in mxd_list:
+        result += item
+
+    return result

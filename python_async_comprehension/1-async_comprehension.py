a96d6efb41f8640b4624177c9d0e75885799298f
WololoRC <wololoRC@proton.me>
2023-09-19 20:03:51 -0300

    task1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhton3
+""" Task 01 """
+import asyncio
+from typing import List
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """ Returns a comprehsed list from a asyn generator """
+    return [item async for item in async_generator()]

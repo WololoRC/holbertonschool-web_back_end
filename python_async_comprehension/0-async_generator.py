875c99b0e4cfedbc3389dce1a64e310eb311fd5c
WololoRC <wololoRC@proton.me>
2023-09-19 19:38:35 -0300

    ?

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ Task 00 """
 import asyncio
 import random
-from typing import AsyncIterator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> AsyncGenerator:
     """ A async generator of 10 random numbers between 0/10 """
     for i in range(10):
         await asyncio.sleep(1)

54ffd2d007f667439ec86d92b7981ca01837e324
WololoRC <wololoRC@proton.me>
2023-09-19 21:04:00 -0300

    task2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,13 +7,9 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 async def measure_runtime() -> float:
     """ measure the time of execute async_comprehnsion 4 times """
+    task_list = [async_comprehension() for i in range(4)]
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
+    await asyncio.gather(*task_list)
     end = time.time()
 
     return end - start

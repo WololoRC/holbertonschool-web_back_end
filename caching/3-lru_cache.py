505e52b2d980499e5d54cc1f1314e5b2bb4351de
WololoRC <wololoRC@proton.me>
2023-09-21 15:30:24 -0300

    clean code

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -5,13 +5,17 @@ from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
-    """ a class LRUCache that inherits from BaseCaching and is a caching system """
+    """ a class LRUCache that inherits from BaseCaching
+         and is a caching system
+     """
+
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Put new items in cahce, if len(self.cache_Data) >= MAX_ITEMS discard 
+        """ Put new items in cahce.
+            If len(self.cache_Data) >= MAX_ITEMS discard
             the first item on self.cache_data.
          """
         if key and item:
@@ -19,9 +23,10 @@ class LRUCache(BaseCaching):
                 rm = self.cache_data.popitem(last=False)
                 print(f"DISCARD: {rm[0]}")
             self.cache_data.update({key: item})
-    
+
     def get(self, key):
-        """ Get a specific item of self.cache_data and put it on the end  
+        """ Get a specific item of self.cache_data
+            and put it on the end
             of self.cache_data dict.
         """
         if key in self.cache_data:

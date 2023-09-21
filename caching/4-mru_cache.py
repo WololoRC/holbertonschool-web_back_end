91bb0e29c979a8bc28d6c5003d89a37503c35b52
WololoRC <wololoRC@proton.me>
2023-09-21 16:12:42 -0300

    tsk4

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" MRU is amazing! """
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """ a class MRUCache that inherits from BaseCaching
+        and is a caching system.
+    """
+    def __init__(self):
+        super().__init__()
+        self.mru_order = []
+
+    def put(self, key, item):
+        """ Put new elements at the begining of self.cache_data.
+            If self.cache_Data >= MAS_ITEMS, pop the first item.
+        """
+        if item and key:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                rm = self.cache_data.pop(self.mru_order[0])
+                print(f"DISCARD: {self.mru_order[0]}")
+                self.mru_order.pop(0)
+            self.cache_data.update({key: item})
+            self.mru_order.insert(0, key)
+
+    def get(self, key):
+        """ Get a specific item od self.cache_Data and move it to
+            the begining.
+        """
+        if key in self.cache_data:
+            self.mru_order.insert(0, key)
+            return self.cache_data.get(key)

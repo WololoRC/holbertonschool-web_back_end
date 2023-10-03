fdf48268bd731d3bb1eb3d323557fea07ef6e8bf
WololoRC <wololoRC@proton.me>
2023-10-03 12:04:41 -0700

    task3

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,8 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """ Class for manage the API authentication.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Do something
         Returns:

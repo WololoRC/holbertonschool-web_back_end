1fca2f20cadf260af255c989e24da7eef9961c3b
WololoRC <wololoRC@proton.me>
2023-10-03 11:30:40 -0700

    task2

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -30,6 +30,15 @@ def stats() -> str:
 def unauthorized():
     """ GET /api/v1/unauthorized
     Return:
-      - Raise a 401 error by using abort
+      - Raise a 401 error
     """
     return abort(401)
+
+
+@app_views.route("forbidden", methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+      - Raise a 403 error
+    """
+    return abort(403)

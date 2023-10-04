b5f6ed34726ece032fc8c7a3a3bfb946f7c60989
WololoRC <wololoRC@proton.me>
2023-10-04 11:35:05 -0700

    task9

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,21 @@ class BasicAuth(Auth):
             return decode
         except Exception:
             return None
+
+    def extract_user_credentials(
+                                self, decoded_base64_authorization_header: str
+                                ) -> (str, str):
+        """
+        returns the use email and password
+        from base64 decoded values
+        """
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        decode_this = decoded_base64_authorization_header.split(':')
+        return decode_this[0], decode_this[1]

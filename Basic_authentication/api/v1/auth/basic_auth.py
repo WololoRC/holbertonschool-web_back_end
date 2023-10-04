bbfc0de2861b3d4ba912b60cfd1aead70d722002
WololoRC <wololoRC@proton.me>
2023-10-04 11:13:54 -0700

    task7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ BasicAuth module """
 from .auth import Auth
-import base64
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -23,3 +23,17 @@ class BasicAuth(Auth):
         base_this = authorization_header.split(' ')
 
         return base_this[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decode the base64 part of @base64_authorization_header """
+
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        encode = base64_authorization_header.encode('utf-8')
+        decode = b64decode(encode)
+        return decode

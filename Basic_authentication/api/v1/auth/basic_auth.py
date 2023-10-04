327b4f23e8e6d33bb4de6af55a2c6a2fce5e4e22
WololoRC <wololoRC@proton.me>
2023-10-04 10:53:58 -0700

    tsk7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 """ BasicAuth module """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
-    """ Nothing by the moment """
+    """ Create basic authentication for requests """
+
+    def extract_base64_authorization_header(self, authorization_header: str
+                                            ) -> str:
+        """ Returns the Base64 for @authorization_header """
+
+        if authorization_header is None:
+            return None
+
+        if type(authorization_header) is not str:
+            return None
+
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        base_this = authorization_header.split(' ')
+
+        return base_this[1]
